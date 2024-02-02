abdbd1d1ecf63cbe0af9005d7f761eb643bd6603
Slav-XpXz <Nightmareboi000@gmail.com>
2024-02-01 21:54:44 -0500

    Started writing the file opening for main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    printf("Hello World! x2 \n");
-    return 0;
+
+int main(int argc, char **argv[]){
+    FILE* input_file;
+    if(argc<2){
+        fprintf(stderr, "Error: No input files!");
+        return 1;
+    }
+    input_file = fopen(argv[2], "r");
+    if(input_file==NULL){
+        fprintf(stderr, "Error: file could not be opened!");
+    }
 }
